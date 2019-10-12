6860e17cf5f5f65e1388652f02941924102fae59
lihu <2840119419@qq.com>
2019-10-12 16:02:05 +0800

    springcloud hystrix dashboard

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public ServletRegistrationBean<HystrixMetricsStreamServlet> getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
